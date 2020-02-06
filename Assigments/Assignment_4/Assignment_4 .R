fcdd40238c1e355ee66814b8239bad108e271afa
Kelly Cook <kellytroconis@Kellys-MacBook-Pro.local>
2020-02-05 19:49:55 -0700

    addedmoresummary

--- Assigments/Assignment_4/Assignment_4 .R
+++ Assigments/Assignment_4/Assignment_4 .R
@@ -3,11 +3,13 @@ library(tidyverse)
 df <- read.delim("./Data/ITS_mapping.csv")
 
 summary(df)
+summary(df$Lat)
+summary(df$Lat)[c(3,5)]
 glimpse(df)
 
 
 table(df$Ecosystem)
-
+plot(x=df$Ecosystem,y=df$Lat, col=df$Ecosys_Type)
 
 png(filename = "./silly_boxplot.png")
 plot(x=df$Ecosystem,y=df$Lat)
