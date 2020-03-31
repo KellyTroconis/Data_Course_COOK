6458565f68be1743a466bf22cc11a79812b69fbe
Kelly Cook <kellytroconis@Kellys-MacBook-Pro.local>
2020-03-30 20:30:17 -0600

    ass8redo

---
+++ Assigments/Assignment_8/Assignment_8.R
@@ -0,0 +1,73 @@
+library(modelr)
+library(broom)
+library(dplyr)
+library(fitdistrplus)
+library(tidyr)
+
+##note:this is the clean(short) version. You can see the "dratf" in assigment_8_practice 
+#1.  loads the "/Data/mushroom_growth.csv" data set
+
+df <- read.csv("./../Data/mushroom_growth.csv")
+
+#2.  creates several plots exploring relationships between the response and predictors
+
+df %>% ggpairs()
+glimpse(df)
+
+#3.  defines at least 2 models that explain the **dependent variable "GrowthRate"**
+# + One must be a lm() and 
+#+ one must be an aov()
+mod1 <-  lm(GrowthRate ~ Light, data = df)
+summary(mod1)
+plot(df$GrowthRate ~ df$Light * df$Species) + abline(mod1, col="Blue")
+
+mod2 <-  lm(GrowthRate ~ Nitrogen, data = df)
+plot(df$GrowthRate ~ df$Nitrogen) + abline(mod2, col="Blue")
+
+mod3 <- lm(GrowthRate ~ Humidity, data = df)
+plot(df$GrowthRate ~ df$Humidity) + abline(mod3, col="Blue")
+
+mod4 <-  aov(GrowthRate ~ Light, data = df)
+plot(df$GrowthRate ~ df$Light) + abline(mod4, col="Blue")
+
+mod5 <-  aov(GrowthRate ~ Nitrogen, data = df)
+plot(df$GrowthRate ~ df$Nitrogen) + abline(mod5, col="Blue")
+
+mod6 <- aov(GrowthRate ~ Humidity, data = df)
+plot(mod6) + abline(mod6, col="Blue")
+
+#4.  calculates the mean sq. error of each model
+m1 <- mean(mod1$residuals^2)
+m2 <- mean(mod2$residuals^2)
+m3 <- mean(mod3$residuals^2)
+m4 <- mean(mod4$residuals^2)
+m5 <- mean(mod5$residuals^2)
+m6 <- mean(mod6$residuals^2)
+
+#5.  selects the best model you tried
+### lm: mod1 
+
+#6.  adds predictions based on new values for the independent variables used in your model
+preds1 <-  add_predictions(df, mod1) 
+ggplot(preds1, aes(x=Light, y=pred, color=Species)) +
+  geom_point()
+
+#preds2 <-  add_predictions(df, mod6) 
+ggplot(preds1, aes(x=Humidity, y=pred, color=Species)) +
+  geom_point()
+
+#7.  plots these predictions alongside the real data
+newdf <-  data.frame(Light = c(5,15,25))
+predictions <-  predict(mod1, newdata = newdf)
+{plot(df$GrowthRate ~ df$Light, xlim=c(0,25),ylim=c(50,200))
+  points(x=newdf$Light, y=predictions, col="Red")
+  abline(mod1)}
+
+#ggplot(preds2, aes(x=GrowthRate, y=pred)) +
+  #geom_point()
+  
+#1.  Are any of your predicted response values from your best model scientifically meaningless? Explain.
+#a small p-value means that we can reject the null hypothesis.There's a relationship between Growthrate and Light (Pr(>t)= 1.65e-12 ***). This makes it not meaningless. 
+
+#2.  In your plots, did you find any non-linear relationships?  If so, do a bit of research online and give a link to at least one resource explaining how to deal with this in R
+# GrowthRate ~ Humidity is not linear. I am still working on undertanding anova. https://www.guru99.com/r-anova-tutorial.html
---
+++ Assigments/Assignment_8/assigment_8_Practice.R
@@ -0,0 +1,116 @@
+library(modelr)
+library(broom)
+library(dplyr)
+library(fitdistrplus)
+library(tidyr)
+library(tidyverse)
+library(GGally)
+library(lindia) #diagnostic for plots lm gg_diagnose
+library(skimr)
+library(patchwork) #add plots together
+library(caret) #to train models 
+(.packages())
+getwd()
+
+#1.  loads the "/Data/mushroom_growth.csv" data set
+
+df <- read.csv("./../Data/mushroom_growth.csv")
+
+#2.  creates several plots exploring relationships between the response and predictors
+
+df %>% ggpairs()
+glimpse(df)
+
+df$Light <- as.numeric(df$Light)
+df$Nitrogen <- as.factor(df$Nitrogen)
+df$Temperature <- as.factor(df$Nitrogen)
+#3.  defines at least 2 models that explain the **dependent variable "GrowthRate"**
+# + One must be a lm() and 
+#+ one must be an aov()
+
+modA <-  lm(GrowthRate ~ Light * Humidity * Nitrogen * Temperature * Species, data = df)
+summary(modA)
+plot(df$GrowthRate ~ df$Light * df$Humidity * df$Nitrogen * df$Temperature * df$Species) + abline(modA, col="Blue")
+
+
+modB <-  lm(GrowthRate ~ Light * Nitrogen * Species, data = df)
+summary(modB)
+plot(df$GrowthRate ~ df$Light * df$Nitrogen * df$Species) + abline(modB, col="Blue")
+
+modC <-  lm(GrowthRate ~ Nitrogen, data = df)
+summary(modC)
+plot(df$GrowthRate ~ df$Nitrogen) + abline(modC, col="Blue")
+
+modD <-  lm(GrowthRate ~ Temperature, data = df)
+summary(modD)
+plot(df$GrowthRate ~ df$Temperature) + abline(modD, col="Blue")
+
+
+mod1 <- aov(GrowthRate ~ Humidity * Light * Species, data = df)
+mod2 <- aov(GrowthRate ~ Nitrogen, data = df)
+mod3 <- aov(GrowthRate ~ Light * Temperature * Species, data = df)
+mod4 <- aov(GrowthRate ~ Temperature, data = df)
+mod5 <- aov(GrowthRate ~ Light * Humidity * Nitrogen * Temperature * Species, data = df)
+#4.  calculates the mean sq. error of each model
+
+mA <- mean(modA$residuals^2)
+mB <- mean(modB$residuals^2)
+mC <- mean(modC$residuals^2)
+mD <- mean(modD$residuals^2)
+m1 <- mean(mod1$residuals^2)
+m2 <- mean(mod2$residuals^2)
+m3 <- mean(mod3$residuals^2)
+m4 <- mean(mod4$residuals^2)
+m5 <- mean(mod5$residuals^2)
+
+#5.  selects the best model you tried
+# from lm: modA, modB. from aov: mod1, mod5
+
+#6.  adds predictions based on new values for the independent variables used in your model
+predsA <-  add_predictions(df, modA) 
+ggplot(predsA, aes(x=Light, y=pred, color=Species)) +
+  geom_point()
+
+predsB <-  add_predictions(df, modB) 
+ggplot(predsB, aes(x=Light, y=pred, color=Species)) +
+  geom_point()
+
+preds1 <-  add_predictions(df, mod1) 
+ggplot(preds1, aes(x=Light, y=pred, color=Species)) +
+  geom_point()
+
+preds5 <-  add_predictions(df, mod5) 
+ggplot(preds5, aes(x=Light, y=pred, color=Species)) +
+  geom_point()
+
+#7.  plots these predictions alongside the real data
+# Make a new dataframe with the predictor values we want to assess
+# mod1 only has "disp" as a predictor so that's what we want to add here
+
+newdf <-  data.frame(Light = c(25,30,35), Humidity= 'High', Nitrogen= '20', Temperature= '20', Species= 'P.ostreotus')
+predictions <-  predict(modA, newdata = newdf)
+
+
+{plot(df$GrowthRate ~ df$Light * df$Humidity * df$Nitrogen * 
+       df$Temperature * df$Species, xlim=c(0,60),ylim=c(-5,220)) +
+  points(x=newdf$Light * newdf$Humidity * newdf$Nitrogen * newdf$Temperature * newdf$Species, y=predictions, col="Red")
+  abline(modA, col="Blue")}
+
+
+
+# plot those predictions on our original graph
+{plot(mtcars$mpg ~ mtcars$disp,xlim=c(0,1000),ylim=c(-10,50))
+  points(x=newdf$disp,y=predictions, col="Red")
+  abline(mod1)}
+
+p1 <- ggplot(df_mod1,aes(x=Sepal.Width,color=Species)) +
+  geom_point(aes(y=Sepal.Length),alpha=.5,size=2) +
+  geom_point(aes(y=pred),color="Black") + theme_bw()
+
+ggplot(aes(x=wt,color=factor(cyl))) +
+  geom_point(aes(y=mpg)) +
+  geom_smooth(method = "lm",aes(y=pred)) +
+  labs(title = "wt + cyl")
+
+
+
